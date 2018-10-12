a97f204305bd67cbe76ebddf76786388d94401c2
Laszlo Hordos <laszlohordos@users.noreply.github.com>
2018-10-12 16:45:45 +0200

    First working version
    First working version

--- graph-ingest-server.js
+++ graph-ingest-server.js
@@ -1,6 +1,114 @@
-module.exports = function (RED) {
-  function RemoteServerNode (n) {
-    RED.nodes.createNode(this, n)
-  }
-  RED.nodes.registerType('openam-server', RemoteServerNode)
+'use strict'
+const rp = require('request-promise')
+module.exports = function(RED) {
+    function GraphIngestServerNode(config) {
+        RED.nodes.createNode(this, config)
+        const node = this
+
+        if (this.credentials.clientId &&
+            this.credentials.clientSecret &&
+            this.credentials.username &&
+            this.credentials.password &&
+            config.tokenEndpoint &&
+            config.tokenEndpoint) {
+
+            node.tokenOps = {
+                method: 'POST',
+                uri: config.tokenEndpoint,
+                form: {
+                    'grant_type': 'password',
+                    username: node.credentials.username,
+                    password: node.credentials.password
+                },
+                auth: {
+                    user: node.credentials.clientId,
+                    pass: node.credentials.clientSecret
+                },
+                json: true
+            }
+            if (typeof config.scope === 'string' && !config.scope === '') {
+                node.tokenOps.form.scope = config.scope
+            }
+
+            node.shouldRefresh = 0
+
+            node.ingestOption = {
+                method: 'POST',
+                uri: `${config.ingestEndpoint}/${config.clusterName}/update`,
+                headers: {
+                    'content-type': 'application/json'
+                },
+                json: true
+            }
+        }
+        this.on('close', function(removed, done) {
+            //TODO Revoke refresh token
+            if (removed) {
+                // This node has been deleted
+            } else {
+                // This node is being restarted
+            }
+            done()
+        })
+
+    }
+    RED.nodes.registerType('graph-ingest-server', GraphIngestServerNode, {
+        credentials: {
+            username: {
+                type: "text"
+            },
+            password: {
+                type: "password"
+            },
+            clientId: {
+                type: "text"
+            },
+            clientSecret: {
+                type: "password"
+            }
+        }
+    })
+
+    GraphIngestServerNode.prototype.getToken = function() {
+        const node = this
+        if (!node.tokenOps) {
+            return Promise.reject(Error('invalid graph-ingest-server configuration'))
+        }
+        if (false) {
+            return Promise.resolve('ee75254b-80c8-4a36-a5cc-24d2bbdff93f')
+        }
+        if (!node.tokenPromise || (new Date()).getTime() > node.shouldRefresh) {
+            node.tokenPromise = rp(node.tokenOps).then(token => {
+                if ('expires_in' in token) {
+                    node.shouldRefresh = ((new Date()).getTime() / 1000 + token['expires_in'] - 300) * 1000;
+                }
+                if ('refresh_token' in token) {
+                    node.refreshOps = {
+                        ...node.tokenOps,
+                        ...{
+                            form: {
+                                'grant_type': 'refresh_token',
+                                'refresh_token': token['refresh_token']
+                            }
+                        }
+                    }
+                }
+                return token['access_token']
+            })
+        }
+        return node.tokenPromise
+    }
+    GraphIngestServerNode.prototype.getIngestOptions = function() {
+        const node = this
+        return this.getToken().then(accessToken => {
+            return {
+                ...node.ingestOption,
+                ...{
+                    auth: {
+                        'bearer': accessToken
+                    }
+                }
+            }
+        })
+    }
 }
--- graph-ingest.js
+++ graph-ingest.js
@@ -1,28 +1,140 @@
-module.exports = function (RED) {
-  function GraphIngest (config) {
-    RED.nodes.createNode(this, config)
-    var node = this
+'use strict'
+const rp = require('request-promise')
+module.exports = function(RED) {
+    function GraphIngest(config) {
+        RED.nodes.createNode(this, config)
+        const node = this
+        node.server = RED.nodes.getNode(config.server)
+        this.on('input', msg => {
+            try {
+                node.status({
+                    fill: 'blue',
+                    shape: 'dot',
+                    text: ' '
+                })
+                const policyName = msg.payload.policyName || config.policyName
+                let ingestEvent = null
 
-    node.server = RED.nodes.getNode(config.server)
+                if (typeof msg.payload === 'object') {
+                    if (typeof msg.payload.update === 'object') {
+                        ingestEvent = msg.payload.update
+                    } else {
+                        const objectId = msg.payload.objectId || msg.payload.id
+                        const objectType = msg.payload.objectType || msg.payload.type
+                        const newObject = msg.payload.newObject
+                        const oldObject = msg.payload.oldObject
 
-    var node = this
+                        if (objectId || objectType) {
+                            if (typeof newObject === 'object') {
+                                const changes = {}
+                                Object.keys(newObject).forEach(function(key) {
+                                    const value = newObject[key]
+                                    //https://neo4j.com/docs/developer-manual/current/cypher/syntax/values/
+                                    if (typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean') {
+                                        changes[key] = {
+                                            value,
+                                            change: 'SET'
+                                        }
+                                    } else if (typeof value === 'object' && 'id' in value) {
+                                        changes[key] = {
+                                            change: 'SET',
+                                            id: value.id
+                                        }
+                                        if (typeof value.type === 'string') {
+                                            changes[key].type = value.type
+                                        }
+                                        if (value.direction === 'INCOMING' || value.in === true) {
+                                            changes[key].direction = 'INCOMING'
+                                        }
+                                        if (value.direction === 'OUTGOING' || value.out === true) {
+                                            changes[key].direction = 'OUTGOING'
+                                        }
+                                        if (typeof value.properties === 'object') {
+                                            changes[key].properties = value.properties
+                                        }
+                                    }
+                                })
 
-    this.on('input', msg => {
-      try {
-        node.status({ fill: 'blue', shape: 'dot', text: ' ' })
+                                if (typeof oldObject === 'object') {
+                                    Object.keys(oldObject).forEach(function(key) {
+                                        if (!(key in changes)) {
+                                            changes[key] = {
+                                                change: 'DELETE'
+                                            }
+                                            const value = oldObject[key]
+                                            if (typeof value === 'object' && value.id) {
+                                                changes[key].id = value.id
+                                                if (typeof value.type === 'string') {
+                                                    changes[key].type = value.type
+                                                }
+                                            }
+                                        }
+                                    })
+                                }
+                                ingestEvent = {
+                                    update: {
+                                        [objectId]: {
+                                            changes,
+                                            type: objectType,
+                                            change: 'SET'
+                                        }
+                                    }
+                                }
+                            } else if (typeof oldObject === 'object') {
+                                ingestEvent = {
+                                    update: {
+                                        [objectId]: {
+                                            type: objectType,
+                                            change: 'DELETE'
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                if (config.dryRun) {
+                    msg.payload = ingestEvent
+                    node.send([msg]);
+                } else if (ingestEvent) {
+                    node.server.getIngestOptions().then(options => {
+                        return rp({
+                            ...options,
+                            ...{
+                                qs: {
+                                    policyName
+                                },
+                                body: ingestEvent
+                            }
+                        })
+                    }).then(body => {
+                        msg.payload = body
+                        node.send([msg]);
+                    }).catch(e => {
+                        node.status({
+                            fill: 'red',
+                            shape: 'dot',
+                            text: e.message
+                        })
+                        node.error(e)
+                        msg.payload = e
+                        node.send([null, msg]);
+                    })
+                }
+            } catch (err) {
+                node.status({
+                    fill: 'red',
+                    shape: 'dot',
+                    text: err.message
+                })
+                node.error(err)
+                return
+            }
+        })
 
-        // Do the job
-        console.log(msg)
-      } catch (err) {
-        node.status({ fill: 'red', shape: 'dot', text: err.message })
-        node.error(err)
-        return
-      }
-    })
-
-    this.on('close', () => {
-      node.status({})
-    })
-  }
-  RED.nodes.registerType('graph-ingest', GraphIngest)
+        this.on('close', () => {
+            node.status({})
+        })
+    }
+    RED.nodes.registerType('graph-ingest', GraphIngest)
 }
